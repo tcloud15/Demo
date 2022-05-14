d92c7acbbe31f60f295a075322632ad3aaae8e0f
tcloud15 <43187479+tcloud15@users.noreply.github.com>
2022-05-14 21:36:54 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
-        ystem.out.println("Hello, World");
+        Shgfhfghfghystem.out.println("Hello, World");
         ystem.out.println("Hello, World");
     }
 
