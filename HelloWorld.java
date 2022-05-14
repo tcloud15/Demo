a056e349e491e8dbde7131a6b4a7c36ff69102c0
me <nguyenvu@plu.edu>
2022-05-14 21:41:18 +0700

    fix

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
-        Shgfhfghfghystem.out.println("Hello, World");
+		Shgfhfghfghystem.out.println("Hello, World");
+        ysdfsdgsfgstem.out.println("Hello, World");
         ystem.out.println("Hello, World");
     }
 
